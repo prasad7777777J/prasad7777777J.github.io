c53eeca5d647637a32ceef1b304e3b24676bac0b
prasad7777777J <psai28071@gmail.com>
2026-03-08 22:01:32 -0600

    next.congig

--- next.config.js
+++ next.config.js
@@ -1,21 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  // Allow the sequence folder to be served as static assets
-  async headers() {
-    return [
-      {
-        source: '/sequence/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
   },
 };
 
