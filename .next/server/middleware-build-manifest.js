31601093c603d96a9a5451e0f3fafca9fea3debd
prasad7777777J <psai28071@gmail.com>
2026-03-08 22:08:26 -0600

    my portfolio

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/d5c2dacd8983f39c.js",
+    "static/chunks/f2f58a7e93290fbb.js",
+    "static/chunks/2ce72b8c3205a669.js",
+    "static/chunks/turbopack-10cb0388d3645cf5.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zinZN/HOOw+lhatpsj1lQuUdQJ76yHzPvqtCZAqF9Xc=\"\n}"
